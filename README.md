895f4653c856648631200686e4b36d7c28ea89d8
Juggle6 <juggle6@users.noreply.github.com>
2023-03-01 18:35:40 +0100

    Set up a basic React app with Vite

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# casual-programming
+# Casual Programming
+Casual programming is simply a project where we program some random things without a plan, and see where we end up.
+
+## Things the website features:
+- Ultimate guide to designing things
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="dramatic-header">
+        <div className='dramatic-header text'>
+          <h1>The Art of Designing Stuff</h1>
+          <p>Created by the most knowledgeable knowledge knowers in the world.</p>
+        </div>
+    </div>
+  )
+}
+
+export default App
