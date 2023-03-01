1c130b845d6ac48dff41db6931f453ce4cbbfe94
Juggle6 <juggle6@users.noreply.github.com>
2023-03-01 19:07:34 +0100

    Started work on front page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="dramatic-header">
+        <img src='/images/mclaren-artura.png' />
         <div className='dramatic-header text'>
           <h1>The Art of Designing Stuff</h1>
           <p>Created by the most knowledgeable knowledge knowers in the world.</p>
