daa1f1abcedd23ecda52ac2284d11c6784f47361
onninnonni <125398277+onninnonni@users.noreply.github.com>
2023-03-02 11:54:16 +0000

    MenuBar added and car in the body.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 
 function App() {
   return (
-    <div className="dramatic-header">
-        <img src='/images/mclaren-artura.png' />
+      <div className="dramatic-header">
+        <img className="menu" src="public/images/hamburger.webp"/>
         <div className='dramatic-header text'>
           <h1>The Art of Designing Stuff</h1>
           <p>Created by the most knowledgeable knowledge knowers in the world.</p>
         </div>
-    </div>
+      </div>
   )
 }
 
