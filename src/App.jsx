582a2ba072cd46cdaa8b8f1323c00299b9c7d4a7
Juggle6 <juggle6@users.noreply.github.com>
2023-03-02 19:23:16 +0100

    Centered the home-page text and changed to high-res background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 
 function App() {
-  return (
-      <div className="dramatic-header">
-        <img className="menu" src="/images/hamburger.webp"/>
-        <div className='dramatic-header text'>
-          <h1>The Art of Designing Stuff</h1>
-          <p>Created by the most knowledgeable knowledge knowers in the world.</p>
-        </div>
-      </div>
-  )
+    return (
+        <>
+            <header>
+                <img className="menu" src="/images/hamburger.webp"/>
+            </header>
+            <div className="home">
+                <div className="front-page-center">
+                    <h1>The Art of Designing Stuff</h1>
+                    <p>Created by the most knowledgeable knowledge knowers in the world.</p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App
