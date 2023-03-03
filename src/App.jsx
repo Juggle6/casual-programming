9fb14468e1d5b0fa7aa497c86f0116ce22e3a4bf
onninnonni <125398277+onninnonni@users.noreply.github.com>
2023-03-03 17:13:58 +0000

    Banana

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ function App() {
     return (
         <>
             <header>
-                <img className="menu" src="/images/hamburger.webp"/>
             </header>
             <div className="home">
+                <img className="menu" src='/images/hamburger.svg'/>
+                <div className="dropDownMenu">
+                    <img className="banana" src='/images/spktkpkt-banana.svg'/>
+                </div>
                 <div className="front-page-center">
                     <h1>The Art of Designing Stuff</h1>
                     <p>Created by the most knowledgeable knowledge knowers in the world.</p>
