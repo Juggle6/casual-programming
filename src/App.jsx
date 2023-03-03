3e65b009fbeab0a43fe4a554f87cb7ae0b3127d3
Juggle6 <juggle6@users.noreply.github.com>
2023-03-03 18:42:45 +0100

    Move the Home page into its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
+import Home from './components/home/home.jsx'
 
 function App() {
     return (
         <>
             <header>
             </header>
-            <div className="home">
-                <img className="menu" src='/images/hamburger.svg'/>
-                <div className="dropDownMenu">
-                    <img className="banana" src='/images/spktkpkt-banana.svg'/>
-                </div>
-                <div className="front-page-center">
-                    <h1>The Art of Designing Stuff</h1>
-                    <p>Created by the most knowledgeable knowledge knowers in the world.</p>
-                </div>
-            </div>
+            <Home />
         </>
     )
 }
