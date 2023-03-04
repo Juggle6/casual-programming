b678e36eab5ef7d84bf3b56ec10d422fec2f7615
onninnonni <125398277+onninnonni@users.noreply.github.com>
2023-03-04 19:19:51 +0000

    Added contacts to contacts

--- src/Pages/contactsPage/contactsPage.jsx
+++ src/Pages/contactsPage/contactsPage.jsx
@@ -5,8 +5,18 @@ export default function ContactsPage() {
     return (
         <div>
             <div className="creators">
-                <div className="Onni">Onni</div>
-                <div className="Mathias">Mathias</div>
+                <div className="M">
+                    <h1>M</h1>
+                    JJJ
+                </div>
+                <div className="Onni">
+                    <h1>Onni</h1>
+                    <img className="OnniImg" src="/images/onni.png"/>
+                </div>
+                <div className="Hill-Billy">
+                    <h1>Hill Billy</h1>
+                    <img className="HillBillyImg" src='/images/hillbilly.png'/>
+                </div>
             </div>
         </div>
     )
