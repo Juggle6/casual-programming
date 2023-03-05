9a63f7b38f3d881268e4896ceca09559606fe149
onninnonni <125398277+onninnonni@users.noreply.github.com>
2023-03-05 08:44:31 +0000

    HillBillly

--- src/Pages/contactsPage/contactsPage.jsx
+++ src/Pages/contactsPage/contactsPage.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 
 export default function ContactsPage() {
     return (
-        <div>
+        <div className="position">
             <div className="creators">
                 <div className="M">
                     <h1>M</h1>
                     JJJ
                 </div>
-                <div className="Onni">
-                    <h1>Onni</h1>
-                    <img className="OnniImg" src="/images/onni.png"/>
-                </div>
                 <div className="Hill-Billy">
                     <h1>Hill Billy</h1>
                     <img className="HillBillyImg" src='/images/hillbilly.png'/>
+                </div>
+                <div className="Onni">
+                    <h1>Onni</h1>
+                    <img className="OnniImg" src="/images/onni.png"/>
                 </div>
             </div>
         </div>
