165e1d556f8ee8efe6c871a453fb7a06b638870e
onninnonni <125398277+onninnonni@users.noreply.github.com>
2023-03-07 11:51:10 +0000

    Hamburgermenu on 600px or less

--- src/components/NavBar/menu.jsx
+++ src/components/NavBar/menu.jsx
@@ -2,12 +2,18 @@ import './menu.css'
 
 export default function Menu() {
     return (
-    <div className="navBar">
-        <div className="navItems">
-            <a href="/">Home</a>
-            <a href="/Design">Design</a>
-            <a href="/about">about</a>
-            <a href="/contacts">contacts</a>
+    <div className="navContainer">
+      <div className="navBar">
+          <div className="hamburgerMenu">
+            <img src="/images/hamburger.svg" alt="Burger" />
+          </div>
+              <div className="navItems">
+                <a href="/">Home</a>
+                <a href="/Design">Design</a>
+                <a href="/about">about</a>
+                <a href="/contacts">contacts</a>
+              </div>
         </div>
     </div>
-)}
+    )
+}
